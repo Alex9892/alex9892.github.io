f911111936f07ce0aec813a9bc3610f595eed81e
Alex9892 <deathkiller138@gmail.com>
2019-03-10 18:15:19 -0700

    first

---
+++ index.js
@@ -0,0 +1,41 @@
+/*var a=10%8*3/2; //you can use this to comment and this is not a code
+
+//alert(a)
+
+/*var arrayA = [1, 2, 5, 6, 10] //semicolins not needed
+
+alert(arrayA) //watch spelling because i type too fast for spelling
+arrayA.push("NEW ELEMENT! ! ! ! !")
+alert(arrayA) //!=not &&=and ||=or */
+
+/*var boo = false && true || !true && true
+alert(boo)*/
+
+/*if(a=3) {
+	alert(true)
+} else{
+	alert(false)
+}*/
+
+/*function addSquares(x, y) {
+	return(x*x + y*y)
+}
+
+//alert(addSquares(2, 3));
+
+function concat(str1, str2) {
+	return(str1 + str2);
+}
+
+var newstring = concat("Hello ", "World");
+
+//alert(concat("Hello ", "World"));
+
+alert(newstring);*/
+
+function triProduct(x, y, z) {
+	alert(x*y*z);
+}
+
+//alert(triProduct(10,11,12)) //1320 
+
